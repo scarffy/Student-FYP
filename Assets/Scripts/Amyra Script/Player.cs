980afe17c35b8cd9978b4ad414a7a98d41c20229
nuramyra <96581651+lildaizee@users.noreply.github.com>
2022-01-11 21:52:30 +0800

    feat : player movement

---
+++ Assets/Scripts/Amyra Script/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private PlayerAction playerAction;
+    private Rigidbody rigidBody;
+    private Vector3 movementInput;
+
+    void Awake()
+    {
+        playerAction = new PlayerAction();
+
+        rigidBody = GetComponent<Rigidbody>();
+        if (rigidBody is null)
+            Debug.LogError("Error!");
+    }
+
+    private void OnEnable()
+    {
+        playerAction.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerAction.Player.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        movementInput = playerAction.Player.Movement.ReadValue<Vector3>();
+        rigidBody.velocity = movementInput * speed;
+    }
+}
