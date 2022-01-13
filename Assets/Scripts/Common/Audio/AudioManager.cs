68d0f67f4860096d6147906a8a0953331165e972
scarffy <34879344+scarffy@users.noreply.github.com>
2022-01-13 23:14:00 +0800

    feat : Audio

---
+++ Assets/Scripts/Common/Audio/AudioManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FYP.Data;
+
+namespace FYP {
+    /// <summary>
+    /// This class will get data from scriptable object as source of truth
+    /// </summary>
+    public class AudioManager : Singleton<AudioManager>
+    {
+        [Header("Audio Source")]
+        public AudioSource backgroundSource;
+        public AudioSource sfxSource;
+
+        [Header("Background Music Clip")]
+        public AudioData backgroundMusic;
+
+        IEnumerator Start()
+        {
+            backgroundSource.clip = backgroundMusic.GetAudioClip();
+            yield return PlayBackground();
+        }
+
+        /// <summary>
+        /// Will loop until we stop it.
+        /// Maybe while is better performant?
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator PlayBackground()
+        {
+            yield return new WaitWhile(() => backgroundSource.isPlaying);
+            backgroundSource.clip = backgroundMusic.GetAudioClip();
+            yield return PlayBackground();
+        }
+
+        public void PlaySfx()
+        {
+
+        }
+    }
+}
