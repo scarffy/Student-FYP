dbc7ebcb6bef172d774ae98508b992e377cf70c3
scarffy <34879344+scarffy@users.noreply.github.com>
2022-01-13 10:19:25 +0800

    style : rename Level manager in hierarychy

--- Assets/Scripts/Common/LevelManager.cs
+++ Assets/Scripts/Common/LevelManager.cs
@@ -10,12 +10,16 @@ namespace FYP
         public enum CurrentLoadedScene
         {
             Core,
+            GameUi,
             Credit,
             RegisterLogin,
             Gameplay
         }
 
         public CurrentLoadedScene currentLoadedScene;
+
+        [Space]
+        public Scene scene1;
 
         void Awake()
         {
@@ -27,9 +31,30 @@ namespace FYP
             SceneManager.sceneLoaded += LoadScene;
         }
 
+        /// <summary>
+        /// If there is no specific scene to load, use this
+        /// </summary>
         public void LoadNextLevel()
         {
             Debug.Log("Called load next level");
+            //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+
+            //switch (currentLoadedScene)
+            //{
+            //    case CurrentLoadedScene.Core:
+
+            //        break;
+            //}
+        }
+
+        /// <summary>
+        /// If there is specific scene to load, use this
+        /// </summary>
+        /// <param name="value"></param>
+        public void LoadNextLevel(int value)
+        {
+
         }
 
         void LoadScene(Scene scene, LoadSceneMode mode)
