255ed5c053e243be627543d38226eb739800ac21
nuramyra <96581651+lildaizee@users.noreply.github.com>
2022-01-25 23:11:33 +0800

    feat (wip) : add thirdplayercontroller, fix cinemachine
    - add third player controller
    - add input system for look and jumping
    - fix cinemachine camera but wip

--- Assets/Scripts/Amyra Script/Player.cs
+++ Assets/Scripts/Amyra Script/Player.cs
@@ -5,39 +5,72 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Settings")]
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private float gravityValue;
+
     private PlayerAction playerAction;
-    //private Rigidbody rigidBody;
     private CharacterController characterController;
     private Vector3 movementInput;
+    private InputAction jump;
+    private bool groundedPlayer;
+    private Transform cameraMainTransform;
 
     void Awake()
     {
         playerAction = new PlayerAction();
 
         characterController = GetComponent<CharacterController>();
-
-        //rigidBody = GetComponent<Rigidbody>();
-        //if (rigidBody is null)
-        //    Debug.LogError("Error!");
+        cameraMainTransform = Camera.main.transform;               
     }
 
     private void OnEnable()
     {
         playerAction.Player.Enable();
+
+        //trigged by events
+        playerAction.Player.Jump.performed += DoJump;
+        playerAction.Player.Jump.Enable();
     }
 
     private void OnDisable()
     {
         playerAction.Player.Disable();
+        playerAction.Player.Jump.Disable();
     }
 
     void FixedUpdate()
     {
-        movementInput = playerAction.Player.Movement.ReadValue<Vector3>();
-        //rigidBody.velocity = movementInput * speed;
+        //!This is for player movement
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && movementInput.y < 0)
+        {
+            movementInput.y = 0f;
+        }
 
+        //movementInput = playerAction.Player.Movement.ReadValue<Vector3>();
+        
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
+        //move.y = 0f;
         characterController.Move(movementInput * Time.deltaTime);
+
+        movementInput.y += gravityValue * Time.deltaTime;
+
+    }
+
+    private void DoJump(InputAction.CallbackContext obj)
+    {
+        if (groundedPlayer)
+        {
+            movementInput.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            Debug.Log("Jump");
+        }
+        
+
     }
 }
