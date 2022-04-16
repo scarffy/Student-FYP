0098cea8f1c3b7c05ef3e9f191b24750b659a319
SharifahNajihah <sharifahnajihah99@gmail.com>
2022-04-16 22:33:21 +0800

    feat : get json

--- Assets/Scripts/Mechanic/Playfab/PlayerStats.cs
+++ Assets/Scripts/Mechanic/Playfab/PlayerStats.cs
@@ -7,16 +7,22 @@ using PlayFab.ClientModels;
 
 namespace FYP.Backend
 {
-    public class PlayerStats : Singleton<PlayerStats>
+    [System.Serializable]
+    public class PlayerData
     {
-        //public UserDataPermission userData;
+        [Header("Player Stats")]
         public int playerLevel;
         public int playerStrength;
         public int playerHealth;
         public int playerVitality;
         public int currentEXP;
         public int maxEXP;
+    }
+    public class PlayerStats : Singleton<PlayerStats>
+    {
+        public PlayerData playerData;
 
+        #region getdata
         public void GetUserData(string myPlayFabId)
         {
             PlayFabClientAPI.GetUserData(new GetUserDataRequest()
@@ -25,12 +31,9 @@ namespace FYP.Backend
                 Keys = null
             }, result =>
             {
-                Backend.PlayFabManager.Instance.playerLevel.text = "Level : " + result.Data["PlayerLevel"].Value;
-                Backend.PlayFabManager.Instance.playerHealth.text = "Health : " + result.Data["PlayerHealth"].Value;
-                Backend.PlayFabManager.Instance.playerVitality.text = "Vitality : " + result.Data["PlayerVitality"].Value;
-                Backend.PlayFabManager.Instance.playerStrength.text = "Strength : " + result.Data["PlayerStrength"].Value;
-                Backend.PlayFabManager.Instance.currentEXP.text = "Current Exp : " + result.Data["CurrentEXP"].Value;
-                Backend.PlayFabManager.Instance.maxEXP.text = " Max Exp : " + result.Data["MaxEXP"].Value;
+                string json = result.Data["PlayerData"].Value;
+                playerData = JsonUtility.FromJson<PlayerData>(json);
+                Backend.PlayFabManager.Instance.playerLevel.text = playerData.playerLevel.ToString();
             }, (error) =>
             {
                 Debug.Log("Got error retrieving user data:");
@@ -38,17 +41,18 @@ namespace FYP.Backend
             });
         }
 
+        #endregion
+
+        #region setdata
         public void SetUserData()
         {
+            string json = JsonUtility.ToJson(playerData);
             PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
             {
                 Data = new Dictionary<string, string>() {
-            {"CurrentEXP", "100"},
-            {"MaxEXP", "200"},
-            {"PlayerHealth", "100"},
-            {"PlayerVitality", "50"},
-            {"PlayerStrength", "100"},
-            {"PlayerLevel", "1"}
+            {"PlayerData", json }
+
+                    
 
         }
             },
@@ -60,10 +64,9 @@ namespace FYP.Backend
             {
                 Debug.Log("Got error setting user statistic data");
                 Debug.Log(error.GenerateErrorReport());
-            });
+            }); ;
         }
-
-
+        #endregion
     }
 }
 
